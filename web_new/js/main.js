1b50426d95c1361cdb43ea532a37a1037b249065
e-cm <ericchiassonmorgan@gmail.com>
2019-02-07 18:04:06 -0500

    started implementing portfolio grid

--- web_new/js/main.js
+++ web_new/js/main.js
@@ -1,3 +1,9 @@
-function frame() {
-	console.log('pffft');
+// Makes each project frame a square
+function squareFrame() {
+	let canvas = document.getElementById('canvas');
+    let squareWidth = document.getElementsByClassName('project')[0].offsetWidth;
+    canvas.style.gridAutoRows = squareWidth + 'px';
 }
+
+window.addEventListener('load', squareFrame, false);
+window.addEventListener('resize', squareFrame, false);
