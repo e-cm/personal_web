123b33ea07a1a947a3c3c065a509d925b1d63ada
e-cm <ericchiassonmorgan@gmail.com>
2019-02-23 17:26:59 -0500

    edited grunt

--- web_new/Gruntfile.js
+++ web_new/Gruntfile.js
@@ -6,29 +6,55 @@ module.exports = function(grunt) {
 	      src: ['js/fades.js', 'js/work.js'],
 	      dest: 'build/js/built_work.js',
 	    },
+	    js_about: {
+	      src: ['js/fades.js', 'js/about.js'],
+	      dest: 'build/js/built_about.js',
+	    },
+	    js_project: {
+	      src: ['js/fades.js', 'js/lightbox-plus-jquery.js'],
+	      dest: 'build/js/built_project.js',
+	    },
 	    es5_work: {
 	      src: ['es5/fades.js', 'es5/work.js'],
 	      dest: 'build/es5/built_work.js',
+	    },
+	    es5_about: {
+	      src: ['es5/fades.js', 'es5/about.js'],
+	      dest: 'build/js/built_about.js',
+	    },
+	    es5_project: {
+	      src: ['es5/fades.js', 'es5/lightbox-plus-jquery.js'],
+	      dest: 'build/js/built_project.js',
 	    },
 	    css_work: {
 	      src: ['css/main.css', 'css/work.css'],
 	      dest: 'build/css/built_work.css',
 	    },
-	  },
-	  watch: {
-	    js: {
-	      files: ['js/**/*.js'],
-	      tasks: ['concat:js_work'],
+	    css_about: {
+	      src: ['css/main.css', 'css/about.css'],
+	      dest: 'build/css/built_about.css',
 	    },
-	    css: {
-	      files: ['css/**/*.css'],
-	      tasks: ['concat:css_work'],
+	    css_project: {
+	      src: ['css/main.css', 'css/project.css', 'css/lightbox.css'],
+	      dest: 'build/css/built_project.css',
 	    },
 	  },
+	  //watch: {
+	    //js: {
+	      //files: ['js/**/*.js'],
+	      //tasks: ['concat:js_work'],
+	    //},
+	    //css: {
+	      //files: ['css/**/*.css'],
+	      //tasks: ['concat:css_work'],
+	    //},
+	  //},
 	  uglify: {
 	    build: {
 	      files: {
-	        'build/es5/built_work.min.js': ['build/es5/built_work.js'] 
+	        'build/es5/built_work.min.js': ['build/es5/built_work.js'],
+	        'build/es5/built_about.min.js': ['build/es5/built_about.js'],
+	        'build/es5/built_project.min.js': ['build/es5/built_project.js'], 
 	      }
 	    }
 	  }
@@ -37,6 +63,6 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.registerTask('default', ['concat', 'uglify', 'watch']);
+	grunt.registerTask('default', ['concat', 'uglify']);
 
 };
