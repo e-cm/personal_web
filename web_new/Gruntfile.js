cf4a036793198c7aafbdad3167ab9d7a4187e2b5
e-cm <ericchiassonmorgan@gmail.com>
2019-02-25 17:27:47 -0500

    day 12: more minimizing

--- web_new/Gruntfile.js
+++ web_new/Gruntfile.js
@@ -1,56 +1,28 @@
 module.exports = function(grunt) {
 
 	grunt.initConfig({
-	  concat: {
-	    es5_work: {
-	      src: ['es5/fades.js', 'es5/work.js'],
-	      dest: 'build/es5/built_work.js',
-	    },
-	    es5_about: {
-	      src: ['es5/fades.js', 'es5/about.js'],
-	      dest: 'build/es5/built_about.js',
-	    },
-	    es5_project: {
-	      src: ['es5/fades.js', 'js/lightbox-plus-jquery.js'],
-	      dest: 'build/es5/built_project.js',
-	    },
-	    css_work: {
-	      src: ['css/main.css', 'css/work.css'],
-	      dest: 'build/css/built_work.css',
-	    },
-	    css_about: {
-	      src: ['css/main.css', 'css/about.css'],
-	      dest: 'build/css/built_about.css',
-	    },
-	    css_project: {
-	      src: ['css/main.css', 'css/project.css', 'css/lightbox.css'],
-	      dest: 'build/css/built_project.css',
-	    },
-	  },
-	  //watch: {
-	    //js: {
-	      //files: ['js/**/*.js'],
-	      //tasks: ['concat:js_work'],
-	    //},
-	    //css: {
-	      //files: ['css/**/*.css'],
-	      //tasks: ['concat:css_work'],
-	    //},
-	  //},
 	  uglify: {
 	    build: {
 	      files: {
-	        'build/es5/built_work.min.js': ['build/es5/built_work.js'],
-	        'build/es5/built_about.min.js': ['build/es5/built_about.js'],
-	        'build/es5/built_project.min.js': ['build/es5/built_project.js'], 
+	        'min/js/work.min.js': ['babeled/fades.js', 'babeled/work.js'],
+	        'min/js/about.min.js': ['babeled/fades.js', 'babeled/about.js'],
+	        'min/js/project.min.js': ['babeled/fades.js', 'js/lightbox-plus-jquery.js'], 
 	      }
 	    }
+	  },
+	  cssmin: {
+		  build: {
+		    files: {
+		      'min/css/work.min.css': ['css/main.css', 'css/work.css'],
+		      'min/css/about.min.css': ['css/main.css', 'css/about.css'],
+		      'min/css/project.min.css': ['css/main.css', 'css/project.css', 'css/lightbox.css'],
+		    }
+		  }
 	  }
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.registerTask('default', ['concat', 'uglify']);
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.registerTask('default', ['uglify', 'cssmin']);
 
 };
