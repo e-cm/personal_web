94762daf78cf7f1c7fa19b712a3034ff1a4752f8
e-cm <ericchiassonmorgan@gmail.com>
2019-09-29 12:06:51 -0400

    scroll indicators now display correctly on both mobile and desktop

--- web_new/babeled/scroll.js
+++ web_new/babeled/scroll.js
@@ -1,59 +1,54 @@
 'use strict';
 
 function initScroll() {
-	var canvas = document.getElementsByClassName('canvas')[0];
+	// let canvas = document.getElementsByClassName('canvas')[0];
 
-	scrollEffects('description');
-	scrollEffects('slides');
+	/* create event listeners related to scrolling on project pages*/
+	scrollEffects('canvas', 'centerarrow');
+	scrollEffects('description', 'leftarrow');
+	scrollEffects('slides', 'rightarrow');
 
-	canvas.addEventListener('scroll', function () {
-		setBarSize(canvas);
-		indicateScroll(canvas, 'centerarrow');
-	});
-
+	/* default scroll event listeners for all pages*/
 	window.addEventListener('resize', function () {
-		if (window.innerWidth <= 670) setBarSize(canvas);
+		if (window.innerWidth <= 670) {
+			setBarSize(canvas);
+		}
+	});
+	canvas.addEventListener('scroll', function () {
+		if (window.innerWidth <= 670) {
+			setBarSize(canvas);
+		}
 	});
 }
 
-function scrollEffects(elementID) {
+function scrollEffects(elementID, arrowID) {
 	var element = document.getElementById(elementID);
+	var arrow = document.getElementById(arrowID);
+	var canvas = document.getElementsByClassName('canvas')[0];
+	/* if no element was found by the ID, it is the canvas */
+	if (!element) element = canvas;
 
-	/* check that the element exists*/
-	if (element) {
+	/* check that the element and arrow exists */
+	if (element && arrow) {
 
-		/* only display the scroll arrow if the content overflows the page */
-		if (element.scrollHeight > element.clientHeight) {
-			switch (element) {
-				case document.getElementById('description'):
-					document.getElementById('leftarrow').style.opacity = 1; /* display the arrow */
-					break;
+		/* check if the arrow needs to be displayed */
+		indicateScroll(element, arrow);
 
-				case document.getElementById('slides'):
-					document.getElementById('rightarrow').style.opacity = 1; /* display the arrow */
-					break;
-			}
-		}
-
+		/* whenever the element is scrolled, update the scroll bar and arrow */
 		element.addEventListener('scroll', function () {
-
 			/* resize scroll bars */
 			setBarSize(element);
-
-			switch (element) {
-				case document.getElementById('description'):
-					document.getElementById('leftarrow').style.opacity = 1; /* display the arrow */
-					indicateScroll(element, 'leftarrow'); /* control when the arrow is visible */
-					break;
-
-				case document.getElementById('slides'):
-					indicateScroll(slides, 'rightarrow');
-					break;
-			}
+			/* control when the arrow is visible */
+			indicateScroll(element, arrow);
 		});
 
+		/* recalculate scroll bars and arrows when resizing the window*/
 		window.addEventListener('resize', function () {
-			if (window.innerWidth >= 671) setBarSize(element);
+			/* if the window is in desktop mode, update scroll bars */
+			if (window.innerWidth > 670) {
+				setBarSize(element);
+			}
+			indicateScroll(element, arrow);
 		});
 	}
 }
@@ -62,8 +57,10 @@ function setBarSize(element) {
 	var scrollPercentage = void 0;
 	var barSize = void 0;
 
+	/* calculate the percentage the element has been scrolled*/
 	scrollPercentage = element.scrollTop / (element.scrollHeight - element.clientHeight);
 
+	/* calculate the pixel width of the scroll bar, capping it when the window exceeds 1200px */
 	if (window.innerWidth < 1200) {
 		barSize = scrollPercentage * (window.innerWidth / 2 - 40); /* 40 = border-LR (15) - center logo offset (25) */
 	} else {
@@ -87,14 +84,17 @@ function setBarSize(element) {
 
 // display an indicator that the content is scrollable 
 // only if they haven't yet scrolled
-function indicateScroll(element, indicator) {
-	var arrow = document.getElementById(indicator);
-	if (arrow) {
+function indicateScroll(element, arrow) {
+	/* only display the scroll arrow if the content overflows the page */
+	if (element.scrollHeight > element.clientHeight) {
 		if (element.scrollTop > 0 && arrow.style.opacity == 1) {
 			arrow.style.opacity = 0;
 		} else if (element.scrollTop == 0) {
 			arrow.style.opacity = 1;
 		}
+		/* otherwise don't display it */
+	} else {
+		arrow.style.opacity = 0;
 	}
 }
 
